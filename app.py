158a71ea0d4b78eca40c997373d2273290ad94d7
Ganesh Kumar <rjeekay24@gmail.com>
2023-07-05 21:48:23 +0530

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -19,7 +19,7 @@ JOBS = [
         'id': 3,
         'title': 'Cloud Engineer',
         'location': 'Remote',
-
+        'salary': '$120,000'
     },
     {
         'id': 4,
@@ -33,6 +33,11 @@ JOBS = [
 @app.route("/")
 def hello_world():
     return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
