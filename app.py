472a80151e0f187dafbf66c1ae155bbfc7f6e365
Ganesh Kumar <rjeekay24@gmail.com>
2023-07-05 21:22:58 +0530

    created jobs in app.py and integrated it to the main page using for loop

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Frontend Engineer',
+        'location': 'Chennai,India',
+        'salary': 'Rs. 2,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Backend Engineer',
+        'location': 'Mumbai,India',
+        'salary': 'Rs. 3,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Cloud Engineer',
+        'location': 'Remote',
+
+    },
+    {
+        'id': 4,
+        'title': 'DevOps Engineer',
+        'location': 'New York,USA',
+        'salary': '$150,000'
+    },
+]
+
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
