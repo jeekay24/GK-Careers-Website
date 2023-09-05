4fae95bfdf1ad2849e86ec4bb75ccebe4913d682
Ganesh Kumar <rjeekay24@gmail.com>
2023-09-05 15:33:12 +0530

    commit

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 
 application = Flask(__name__)
+
+application.secret_key = "my previous"
 
 JOBS = [
     {
@@ -35,9 +37,32 @@ def hello_world():
     return render_template('home.html', jobs=JOBS)
 
 
+@application.route("/logout")
+def logout_page():
+    return render_template('logout.html')
+
+
 @application.route("/api/jobs")
 def list_jobs():
     return jsonify(JOBS)
+
+
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials.Please try again'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('hello_world'))
+    return render_template('login.html', error=error)
+
+
+@application.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('logout_page'))
 
 
 if __name__ == "__main__":
